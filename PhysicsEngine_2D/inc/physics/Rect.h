019d9d75c48f3748c2ce9a316771c28b670f419d
Jack <jack.w.mccall@gmail.com>
2018-02-28 03:12:29 +1100

    [2018.2.28] Started coding AABB

--- PhysicsEngine_2D/inc/physics/Rect.h
+++ PhysicsEngine_2D/inc/physics/Rect.h
@@ -7,5 +7,8 @@ namespace physics {
 	public:
 		Rect(ShapeType shape, glm::vec2 position, glm::vec2 size, float mass, glm::vec4 color, bool isStatic);
 		virtual ~Rect();
+
+	protected:
+		glm::vec2 m_min, m_max;
 	};
 }
